94db2174b8227da22b816ff10cb3ab3d4d19beba
Unknown <unknown@unknown>
2000-05-08 19:12:44 +0000

    Modified license, authors, and changed certain references to MC to APBS

--- src/generic/apbs/vatom.h
+++ src/generic/apbs/vatom.h
@@ -1,23 +1,35 @@
 /* ///////////////////////////////////////////////////////////////////////////
-/// MC = < Manifold Code >
-/// Copyright (C) 1993  Michael Holst
-/// Copyright (C) 1999  Contributing authors listed in the code documentation
-///                     retain the copyrights to their code contributions.
+/// APBS -- Adaptive Poisson-Boltzmann Solver
 ///
-/// The program MC is copyrighted software, and there are restrictions on its
-/// use, modification, and distribution; please refer to the document COPYING
-/// which accompanies this software.
+///  Nathan A. Baker (nbaker@wasabi.ucsd.edu)
+///  Dept. of Chemistry and Biochemistry
+///  Dept. of Mathematics, Scientific Computing Group
+///  University of California, San Diego 
 ///
-/// This program is distributed in the hope that it will be useful,
-/// but WITHOUT ANY WARRANTY; without even the implied warranty of
-/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-/// See the document COPYING for more details.
+///  Additional contributing authors listed in the code documentation.
 ///
-/// You should have received a copy of the COPYING file with this program;
-/// if not, write to the author Michael Holst at:  mholst@math.ucsd.edu
-///
-/// rcsid="$Id$"
-/// /////////////////////////////////////////////////////////////////////// */
+/// Copyright © 1999. The Regents of the University of California (Regents).
+/// All Rights Reserved. 
+/// 
+/// Permission to use, copy, modify, and distribute this software and its
+/// documentation for educational, research, and not-for-profit purposes,
+/// without fee and without a signed licensing agreement, is hereby granted,
+/// provided that the above copyright notice, this paragraph and the
+/// following two paragraphs appear in all copies, modifications, and
+/// distributions.
+/// 
+/// IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+/// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+/// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+/// REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+/// 
+/// REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+/// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+/// PARTICULAR PURPOSE.  THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF
+/// ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS".  REGENTS HAS NO OBLIGATION
+/// TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+/// MODIFICATIONS. 
+//////////////////////////////////////////////////////////////////////////// */
 
 /* ///////////////////////////////////////////////////////////////////////////
 // File:     vatom.h    < vatom.c >
