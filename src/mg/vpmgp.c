06dba656f07e1449d0bb4c3ed1e716de802e4e37
Unknown <unknown@unknown>
2000-12-06 17:02:28 +0000

    Added support for Mike's zPMG (FORTRAN) code.  The test case seems to work fine (i.e. reproduces solvation free energy for 3 � Born ion using linearized Poisson-Boltzmann).

---
+++ src/mg/vpmgp.c
@@ -0,0 +1,154 @@
+/* ///////////////////////////////////////////////////////////////////////////
+/// APBS -- Adaptive Poisson-Boltzmann Solver
+///
+///  Nathan A. Baker (nbaker@wasabi.ucsd.edu)
+///  Dept. of Chemistry and Biochemistry
+///  Dept. of Mathematics, Scientific Computing Group
+///  University of California, San Diego 
+///
+///  Additional contributing authors listed in the code documentation.
+///
+/// Copyright © 1999. The Regents of the University of California (Regents).
+/// All Rights Reserved. 
+/// 
+/// Permission to use, copy, modify, and distribute this software and its
+/// documentation for educational, research, and not-for-profit purposes,
+/// without fee and without a signed licensing agreement, is hereby granted,
+/// provided that the above copyright notice, this paragraph and the
+/// following two paragraphs appear in all copies, modifications, and
+/// distributions.
+/// 
+/// IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+/// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+/// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+/// REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+/// 
+/// REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+/// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+/// PARTICULAR PURPOSE.  THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF
+/// ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS".  REGENTS HAS NO OBLIGATION
+/// TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+/// MODIFICATIONS. 
+//////////////////////////////////////////////////////////////////////////// 
+/// rcsid="$Id$"
+//////////////////////////////////////////////////////////////////////////// */
+
+/* ///////////////////////////////////////////////////////////////////////////
+// File:     vpmgp.c
+//
+// Purpose:  Class Vpmgp: methods.
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+
+#include "apbscfg.h"
+#include "apbs/vpmgp.h"
+
+VEMBED(rcsid="$Id$")
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Class Vpmgp: Inlineable methods
+/////////////////////////////////////////////////////////////////////////// */
+#if !defined(VINLINE_VACC)
+#endif /* if !defined(VINLINE_VACC) */
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Class Vpmgp: Non-inlineable methods
+/////////////////////////////////////////////////////////////////////////// */
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  Vpmgp_ctor
+//
+// Purpose:  Construct the PMG parameter object; see header file for more
+//           information
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC Vpmgp* Vpmgp_ctor(int nx, int ny, int nz, int nlev, double hx, 
+  double hy, double hz, int nonlin) {
+
+    Vpmgp *thee = VNULL;
+
+    /* Set up the structure */
+    thee = Vmem_malloc(VNULL, 1, sizeof(Vpmgp) );
+    VASSERT( thee != VNULL);
+    VASSERT(Vpmgp_ctor2(thee, nx, ny, nz, nlev, hx, hy, hz, nonlin));
+
+    return thee;
+}
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  Vpmgp_ctor2
+//
+// Purpose:  Construct the PMG parameter object
+//
+// Notes:    See header files for default parameter values
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC int Vpmgp_ctor2(Vpmgp *thee, int nx, int ny, int nz, int nlev,
+  double hx, double hy, double hz, int nonlin) {
+
+    /* Specified parameters */
+    thee->nx = nx;
+    thee->ny = ny;
+    thee->nz = nz;
+    thee->hx = hx;
+    thee->hy = hy;
+    thee->hz = hz;
+    thee->nlev = nlev; 
+    thee->nonlin = nonlin;
+
+    /* Default parameters */
+    thee->errtol = 1.0e-9;
+    thee->itmax = 100;
+    thee->istop = 1;
+    thee->iinfo = 1;
+    thee->ipkey = 0;
+    thee->key = 0;
+    thee->iperf = 0;
+    thee->meth = 2;
+    thee->mgkey = 0;
+    thee->nu1 = 2;
+    thee->nu2 = 2;
+    thee->mgsmoo = 1;
+    thee->mgprol = 0;
+    thee->mgcoar = 2;
+    thee->mgsolv = 1;
+    thee->mgdisc = 0;
+    thee->omegal = 8.0e-1;
+    thee->omegan = 9.0e-1;
+    thee->ipcon = 3;
+    thee->irite = 8;
+    thee->xcent = 0.0;
+    thee->ycent = 0.0;
+    thee->zcent = 0.0;
+
+    return 1;
+}
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  Vpmgp_dtor
+//
+// Purpose:  Clean up
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC void Vpmgp_dtor(Vpmgp **thee) {
+    
+    if ((*thee) != VNULL) {
+        Vpmgp_dtor2(*thee);
+        Vmem_free(VNULL, 1, sizeof(Vpmgp), (void **)thee);
+        (*thee) = VNULL;
+    }
+
+}
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  Vpmgp_dtor2
+//
+// Purpose:  Clean up
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC void Vpmgp_dtor2(Vpmgp *thee) { ; }
