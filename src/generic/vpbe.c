87e3a972f71fcfd86871f33dafcd00f426938ac6
Unknown <unknown@unknown>
2001-01-09 23:29:01 +0000

    Added some I/O to keep track of Vacc_ctor

--- src/generic/vpbe.c
+++ src/generic/vpbe.c
@@ -505,8 +505,10 @@ VPUBLIC int Vpbe_ctor2(Vpbe *thee, Valist *alist, double ionConc,
     if (thee->ionRadius > thee->solventRadius) radius = thee->ionRadius;
     else radius = thee->solventRadius;
     nhash = 2.0*VPOW((double)Valist_getNumberAtoms(thee->alist), 1.0/3.0);
+    Vnm_print(0, "Vpbe_ctor2: Started constructing Vacc object...\n"); 
     thee->acc = Vacc_ctor(thee->alist, radius, (int)(nhash), (int)(nhash),
       (int)(nhash), 200);
+    Vnm_print(0, "Vpbe_ctor2: Done constructing Vacc object...\n"); 
     VASSERT(thee->acc != VNULL);
 
     return 1; 
