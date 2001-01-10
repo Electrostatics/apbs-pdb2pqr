24f52f390f3aa1a4309b6b212fb3e0ce0bd9f56b
Unknown <unknown@unknown>
2001-01-10 00:53:47 +0000

    Modified Vpbe_ctor to use fewer points in atomic hash tables Modified pnoshell to construct Vpbe objects only as needed and dispose of them when finished.

--- src/generic/vpbe.c
+++ src/generic/vpbe.c
@@ -504,7 +504,7 @@ VPUBLIC int Vpbe_ctor2(Vpbe *thee, Valist *alist, double ionConc,
     /* Compute accessibility objects */
     if (thee->ionRadius > thee->solventRadius) radius = thee->ionRadius;
     else radius = thee->solventRadius;
-    nhash = 2.0*VPOW((double)Valist_getNumberAtoms(thee->alist), 1.0/3.0);
+    nhash = VPOW((double)Valist_getNumberAtoms(thee->alist)/5.0, 1.0/3.0);
     Vnm_print(0, "Vpbe_ctor2: Started constructing Vacc object...\n"); 
     thee->acc = Vacc_ctor(thee->alist, radius, (int)(nhash), (int)(nhash),
       (int)(nhash), 200);
