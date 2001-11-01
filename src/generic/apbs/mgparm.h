a857cf3fad800963a1f35b8b3d3f933ab27436e4
Unknown <unknown@unknown>
2001-11-01 23:24:36 +0000

    Finished restructuring.  Automatic focusing works, parallel doesn't.

---
+++ src/generic/apbs/mgparm.h
@@ -0,0 +1,139 @@
+/* ///////////////////////////////////////////////////////////////////////////
+/// APBS -- Adaptive Poisson-Boltzmann Solver
+///
+///  Nathan A. Baker (nbaker@wasabi.ucsd.edu)
+///  Dept. of Chemistry and Biochemistry
+///  Dept. of Mathematics, Scientific Computing Group
+///  University of California, San Diego 
+///
+///  Additional contributing authors listed in the code documentation.
+///
+/// Copyright © 1999. The Regents of the University of California (Regents).
+/// All Rights Reserved. 
+/// 
+/// Permission to use, copy, modify, and distribute this software and its
+/// documentation for educational, research, and not-for-profit purposes,
+/// without fee and without a signed licensing agreement, is hereby granted,
+/// provided that the above copyright notice, this paragraph and the
+/// following two paragraphs appear in all copies, modifications, and
+/// distributions.
+/// 
+/// IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+/// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+/// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+/// REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+/// 
+/// REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+/// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+/// PARTICULAR PURPOSE.  THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF
+/// ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS".  REGENTS HAS NO OBLIGATION
+/// TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+/// MODIFICATIONS. 
+//////////////////////////////////////////////////////////////////////////// 
+/// rcsid="$Id$"
+//////////////////////////////////////////////////////////////////////////// */
+
+/* ///////////////////////////////////////////////////////////////////////////
+// File:     mgparm.h    
+//
+// Purpose:  A set of useful parameters for a generic multigrid calculation
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+
+#ifndef _MGPARM_H_
+#define _MGPARM_H_
+
+#include "apbs/apbs.h"
+#include "maloc/maloc.h"
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Class MGparm: Definition
+//
+// If you add/change something here, it must be added/changed in MGparm_copy
+/////////////////////////////////////////////////////////////////////////// */
+typedef struct MGparm {
+
+    int type;                            /* What type of MG calculation?
+                                          *    0 => sequential manual
+                                          *    1 => sequential auto-focus
+                                          *    2 => parallel auto-focus */
+    int parsed;
+
+    /* *** GENERIC PARAMETERS *** */
+    int dime[3];               /* Grid dimensions */
+    int setdime;
+
+    /* *** TYPE 0 PARAMETERS (SEQUENTIAL MANUAL) *** */
+    int nlev;                  /* DEPRECATED!!!! (Ignored now)
+                                * Levels in multigrid hierarchy */
+    int setnlev;
+    double grid[3];            /* Grid spacings */
+    int setgrid;
+    double glen[3];            /* Grid side lengths. */
+    int setglen;
+    int cmeth;                 /* Centering method: 0 => center on point, 
+                                * 1 => center on molecule */
+    double center[3];          /* Grid center. If ispart = 0, then this is only
+                                * meaningful if cmeth = 0.  However, if ispart
+                                * = 1 and cmeth = 0, then this is the center of
+                                * the non-disjoint (overlapping) partition.  If
+                                * ispart = 1 and cmeth = 1, then this is the
+                                * vector that must be added to the center of
+                                * the molecule to give the center of the
+                                * non-disjoint partition.  */
+    int centmol;               /* Particular molecule on which we want to
+                                * center the grid */
+    int setgcent;  
+
+    /* ******** TYPE 1 & 2 PARAMETERS (SEQUENTIAL & PARALLEL AUTO-FOCUS) *** */
+    double cglen[3];           /* Coarse grid side lengths */
+    int setcglen;
+    double fglen[3];           /* Fine grid side lengths */
+    int setfglen;
+    int ccmeth;                /* Coarse grid centering method:  0 => center on
+                                * point, 1 => center on molecule */
+    double ccenter[3];         /* Coarse grid center.  */
+    int ccentmol;              /* Particular molecule on which we want to
+                                * center the coarse grid */
+    int setcgcent;
+    int fcmeth;                /* Fine grid centering method:  0 => center on
+                                * point, 1 => center on molecule */
+    double fcenter[3];         /* Fine grid center.  */
+    int fcentmol;              /* Particular molecule on which we want to
+                                * center the fine grid */
+    int setfgcent;
+
+
+    /* ********* TYPE 2 PARAMETERS (PARALLEL AUTO-FOCUS) ******** */
+    double partCenterShift[3];           /* When added to the actual (local)
+                                          * mesh center, this gives the center
+                                          * of the disjoint partition */
+    double partCenterLength[3];          /* This gives the lengths of the
+                                          * disjoint partition */
+    int pdime[3];                        /* Grid of processors to be used in
+                                          * calculation */
+    int setpdime;
+    int rank;                            /* Rank of this processor */
+    int setrank;
+    int ofrac;                           /* Overlap fraction between procs */
+    int setofrac; 
+
+} MGparm;
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Class NOsh: Non-inlineable methods (mcsh.c)
+/////////////////////////////////////////////////////////////////////////// */
+
+VEXTERNC MGparm*  MGparm_ctor(int type);
+VEXTERNC int      MGparm_ctor2(MGparm *thee, int type);
+VEXTERNC void     MGparm_dtor(MGparm **thee);
+VEXTERNC void     MGparm_dtor2(MGparm *thee);
+VEXTERNC int      MGparm_check(MGparm *thee);
+VEXTERNC void     MGparm_copy(MGparm *thee, MGparm *parm);
+VEXTERNC int      MGparm_parseToken(MGparm *thee, char tok[VMAX_BUFSIZE], 
+                    Vio *sock);
+
+
+#endif 
+
---
+++ src/generic/pbeparm.c
@@ -0,0 +1,501 @@
+/* ///////////////////////////////////////////////////////////////////////////
+/// APBS -- Adaptive Poisson-Boltzmann Solver
+///
+///  Nathan A. Baker (nbaker@wasabi.ucsd.edu)
+///  Dept. of Chemistry and Biochemistry
+///  Dept. of Mathematics, Scientific Computing Group
+///  University of California, San Diego 
+///
+///  Additional contributing authors listed in the code documentation.
+///
+/// Copyright © 1999. The Regents of the University of California (Regents).
+/// All Rights Reserved. 
+/// 
+/// Permission to use, copy, modify, and distribute this software and its
+/// documentation for educational, research, and not-for-profit purposes,
+/// without fee and without a signed licensing agreement, is hereby granted,
+/// provided that the above copyright notice, this paragraph and the
+/// following two paragraphs appear in all copies, modifications, and
+/// distributions.
+/// 
+/// IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+/// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+/// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+/// REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+/// 
+/// REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+/// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+/// PARTICULAR PURPOSE.  THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF
+/// ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS".  REGENTS HAS NO OBLIGATION
+/// TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+/// MODIFICATIONS. 
+//////////////////////////////////////////////////////////////////////////// 
+/// rcsid="$Id$"
+//////////////////////////////////////////////////////////////////////////// */
+
+/* ///////////////////////////////////////////////////////////////////////////
+// File:     pbeparm.c
+//
+// Purpose:  Class PBEparm: methods. 
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+
+#include "apbscfg.h"
+#include "apbs/pbeparm.h"
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Class PBEparm: Inlineable methods
+/////////////////////////////////////////////////////////////////////////// */
+#if !defined(VINLINE_MGPARM)
+
+#endif /* if !defined(VINLINE_MGPARM) */
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Class PBEparm: Non-inlineable methods
+/////////////////////////////////////////////////////////////////////////// */
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  PBEparm_ctor
+//
+// Author: Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC PBEparm* PBEparm_ctor() {
+
+    /* Set up the structure */
+    PBEparm *thee = VNULL;
+    thee = Vmem_malloc(VNULL, 1, sizeof(PBEparm));
+    VASSERT( thee != VNULL);
+    VASSERT( PBEparm_ctor2(thee) );
+
+    return thee;
+}
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  PBEparm_ctor2
+//
+// Purpose:  Construct the PBEparm object
+//
+// Notes:    Constructor broken into two parts for FORTRAN users.
+//
+// Returns:  1 if sucessful, 0 otherwise
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC int PBEparm_ctor2(PBEparm *thee) {
+
+    int i;
+
+    if (thee == VNULL) return 0;
+
+    thee->parsed = 0;
+
+    thee->setmolid = 0;
+    thee->setnonlin = 0;
+    thee->setbcfl = 0;
+    thee->setnion = 0;
+    for (i=0; i<MAXION; i++) thee->setion[i] = 0;
+    thee->setpdie = 0;
+    thee->setsdie = 0;
+    thee->setsrfm = 0;
+    thee->setsrad = 0;
+    thee->setswin = 0; 
+    thee->settemp = 0;
+    thee->setgamma = 0;
+    thee->setcalcenergy = 0;      
+    thee->setcalcforce = 0;       
+    thee->setwritepot = 0; 
+    thee->setwriteacc = 0; 
+    thee->nion = 0;
+    thee->swin = 0;
+    thee->srad = 1.4;
+
+    return 1; 
+}
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  PBEparm_dtor
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC void PBEparm_dtor(PBEparm **thee) {
+    if ((*thee) != VNULL) {
+        PBEparm_dtor2(*thee);
+        Vmem_free(VNULL, 1, sizeof(PBEparm), (void **)thee);
+        (*thee) = VNULL;
+    }
+}
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  PBEparm_dtor2
+//
+// Purpose:  Destroy the atom object
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC void PBEparm_dtor2(PBEparm *thee) { ; }
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  PBEparm_check
+//
+// Purpose:  Check the parameter settings for internal consistency
+//
+// Returns:  1 if OK, 0 otherwise
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC int PBEparm_check(PBEparm *thee) { 
+
+    int i;
+
+    /* Check to see if we were even filled... */
+    if (!thee->parsed) {
+        Vnm_print(2, "PBEparm_check:  not filled!\n");
+        return 0;
+    }
+
+    if (!thee->setmolid) {
+        Vnm_print(2, "PBEparm_check:  MOL not set!\n");
+        return 0;
+    }
+    if (!thee->setnonlin) {
+        Vnm_print(2, "PBEparm_check:  LPBE or NPBE not set!\n");
+        return 0;
+    }
+    if (!thee->setbcfl) {
+        Vnm_print(2, "PBEparm_check:  BCFL not set!\n");
+        return 0;
+    }
+    if (!thee->setnion) {
+        thee->setnion = 1;
+        thee->nion = 0;
+    } 
+    for (i=0; i<thee->nion; i++) {
+        if (!thee->setion[i]) {
+            Vnm_print(2, "PBEparm_check: ION #%d not set!\n",i);
+            return 0;
+        }
+    }
+    if (!thee->setpdie) {
+        Vnm_print(2, "PBEparm_check: PDIE not set!\n");
+        return 0;
+    }
+    if (!thee->setsdie) {
+        Vnm_print(2, "PBEparm_check: SDIE not set!\n");
+        return 0;
+    }
+    if (!thee->setsrfm) {
+        Vnm_print(2, "PBEparm_check: SRFM not set!\n");
+        return 0;
+    }
+    if (((thee->srfm==0) || (thee->srfm==1)) && (!thee->setsrad)) {
+        Vnm_print(2, "PBEparm_check: SRAD not set!\n");
+        return 0;
+    }
+    if ((thee->srfm==2) && (!thee->setswin)) {
+        Vnm_print(2, "PBEparm_check: SWIN not set!\n");
+        return 0;
+    }
+    if (!thee->settemp) {
+        Vnm_print(2, "PBEparm_check: TEMP not set!\n");
+        return 0;
+    }
+    if (!thee->setgamma) {
+        Vnm_print(2, "PBEparm_check: GAMMA not set!\n");
+        return 0;
+    }
+    if (!thee->setcalcenergy) thee->calcenergy = 0;
+    if (!thee->setcalcforce) thee->calcforce = 0;
+    if (!thee->setwritepot) thee->writepot = 0;
+    if (!thee->setwriteacc) thee->writeacc = 0;
+
+    return 1;
+}
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  PBEparm_copy
+//  
+// Purpose:  Copy parm into thee
+//
+// Args:     parm    object to copy into thee
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC void PBEparm_copy(PBEparm *thee, PBEparm *parm) {
+
+    int i;
+
+    VASSERT(thee != VNULL);
+    VASSERT(parm != VNULL);
+
+    thee->molid = parm->molid;
+    thee->setmolid = parm->setmolid;
+    thee->nonlin = parm->nonlin; 
+    thee->setnonlin = parm->setnonlin;
+    thee->bcfl = parm->bcfl;
+    thee->setbcfl = parm->setbcfl;
+    thee->nion = parm->nion;
+    thee->setnion = parm->setnion;
+    for (i=0; i<MAXION; i++) {
+        thee->ionq[i] = parm->ionq[i];
+        thee->ionc[i] = parm->ionc[i];
+        thee->ionr[i] = parm->ionr[i];
+        thee->setion[i] = parm->setion[i];
+    };
+    thee->pdie = parm->pdie;
+    thee->setpdie = parm->setpdie;
+    thee->sdie = parm->sdie;
+    thee->setsdie = parm->setsdie;
+    thee->srfm = parm->srfm;
+    thee->setsrfm = parm->setsrfm;
+    thee->srad = parm->srad;
+    thee->setsrad = parm->setsrad;
+    thee->swin = parm->swin;
+    thee->setswin = parm->setswin;
+    thee->temp = parm->temp;
+    thee->settemp = parm->settemp;
+    thee->gamma = parm->gamma;
+    thee->setgamma = parm->setgamma;
+    thee->calcenergy = parm->calcenergy;
+    thee->setcalcenergy = parm->setcalcenergy;
+    thee->calcforce = parm->calcforce;
+    thee->setcalcforce = parm->setcalcforce;
+    thee->writepot = parm->writepot;
+    thee->setwritepot = parm->setwritepot;
+    for (i=0; i<VMAX_ARGLEN; i++) thee->writepotstem[i] = parm->writepotstem[i];
+    thee->writepotfmt = parm->writepotfmt;
+    thee->writeacc = parm->writeacc;
+    thee->setwriteacc = parm->setwriteacc;
+    for (i=0; i<VMAX_ARGLEN; i++) thee->writeaccstem[i] = parm->writeaccstem[i];
+    thee->writeaccfmt = parm->writeaccfmt;
+    thee->parsed = parm->parsed;
+
+}
+
+/* ///////////////////////////////////////////////////////////////////////////
+// Routine:  PBEparm_parseToken
+//
+// Purpose:  Parse a PBE keyword
+//
+// Returns:  1 if matched and assigned
+//          -1 if matched, but there's some sort of error (i.e., too few args)
+//           0 if not matched
+//
+// Author:   Nathan Baker
+/////////////////////////////////////////////////////////////////////////// */
+VPUBLIC int PBEparm_parseToken(PBEparm *thee, char tok[VMAX_BUFSIZE], 
+  Vio *sock) {
+
+    int ti;
+    double tf;
+
+    if (thee == VNULL) {
+        Vnm_print(2, "parsePBE:  got NULL thee!\n");
+        return -1;
+    }
+    if (sock == VNULL) {
+        Vnm_print(2, "parsePBE:  got NULL socket!\n");
+        return -1;
+    }
+
+    if (strcasecmp(tok, "mol") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%d", &ti) == 0) {
+            Vnm_print(2, "NOsh:  Read non-int (%s) while parsing MOL \
+keyword!\n", tok);
+            return -1;
+        } 
+        thee->molid = ti;
+        thee->setmolid = 1;
+        return 1;
+    } else if (strcasecmp(tok, "lpbe") == 0) {
+        thee->nonlin = 0;
+        thee->setnonlin = 1;
+        return 1;
+    } else if (strcasecmp(tok, "npbe") == 0) {
+        thee->nonlin = 1;
+        thee->setnonlin = 1;
+        return 1;
+    } else if (strcasecmp(tok, "bcfl") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%d", &ti) == 0) {
+            Vnm_print(2, "NOsh:  Read non-int (%s) while parsing BCFL \
+keyword!\n", tok);
+            return -1;
+        } 
+        thee->bcfl = ti;
+        thee->setbcfl = 1;
+        return 1;
+    } else if (strcasecmp(tok, "ion") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%lf", &tf) == 0) {
+            Vnm_print(2, "NOsh:  Read non-float (%s) while parsing ION \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->ionq[thee->nion] = tf;
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%lf", &tf) == 0) {
+            Vnm_print(2, "NOsh:  Read non-float (%s) while parsing ION \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->ionc[thee->nion] = tf;
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%lf", &tf) == 0) {
+            Vnm_print(2, "NOsh:  Read non-float (%s) while parsing ION \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->ionr[thee->nion] = tf;
+        thee->setion[thee->nion] = 1;
+        (thee->nion)++;
+        thee->setnion = 1;
+        return 1;
+    } else if (strcasecmp(tok, "pdie") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%lf", &tf) == 0) {
+            Vnm_print(2, "NOsh:  Read non-float (%s) while parsing PDIE \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->pdie = tf;
+        thee->setpdie = 1;
+        return 1;
+    } else if (strcasecmp(tok, "sdie") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%lf", &tf) == 0) {
+            Vnm_print(2, "NOsh:  Read non-float (%s) while parsing SDIE \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->sdie = tf;
+        thee->setsdie = 1;
+        return 1;
+    } else if (strcasecmp(tok, "srfm") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%d", &ti) == 0) {
+            Vnm_print(2, "NOsh:  Read non-int (%s) while parsing SRFM \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->srfm = ti;
+        thee->setsrfm = 1;
+        return 1;
+    } else if (strcasecmp(tok, "srad") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%lf", &tf) == 0) {
+            Vnm_print(2, "NOsh:  Read non-float (%s) while parsing SRAD \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->srad = tf;
+        thee->setsrad = 1;
+        return 1;
+    } else if (strcasecmp(tok, "swin") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%lf", &tf) == 0) {
+           Vnm_print(2, "NOsh:  Read non-float (%s) while parsing SWIN \
+keyword!\n", tok);
+           return -1;
+        }
+        thee->swin = tf;
+        thee->setswin = 1;
+        return 1;
+    } else if (strcasecmp(tok, "temp") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%lf", &tf) == 0) {
+            Vnm_print(2, "NOsh:  Read non-float (%s) while parsing TEMP \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->temp = tf;
+        thee->settemp = 1; 
+        return 1;
+    } else if (strcasecmp(tok, "gamma") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%lf", &tf) == 0) {
+            Vnm_print(2, "NOsh:  Read non-float (%s) while parsing GAMMA \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->gamma = tf;
+        thee->setgamma = 1;
+        return 1;
+    } else if (strcasecmp(tok, "calcenergy") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%d", &ti) == 0) {
+            Vnm_print(2, "NOsh:  Read non-int (%s) while parsing \
+WRITEENERGY keyword!\n", tok);
+            return -1;
+        }
+        thee->calcenergy = ti;
+        thee->setcalcenergy = 1;
+        return 1;
+    } else if (strcasecmp(tok, "calcforce") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%d", &ti) == 0) {
+            Vnm_print(2, "NOsh:  Read non-int (%s) while parsing \
+WRITEFORCE keyword!\n", tok);
+            return -1;
+        }
+        thee->calcforce = ti;
+        thee->setcalcforce = 1;
+        return 1;
+    } else if (strcasecmp(tok, "writepot") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%d", &ti) == 0) {
+            Vnm_print(2, "NOsh:  Read non-int (%s) while parsing WRITEPOT \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->writepot = ti;
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (strcasecmp(tok, "dx") == 0) {
+            thee->writepotfmt = 0;
+        } else if (strcasecmp(tok, "avs") == 0) {
+            thee->writepotfmt = 1;
+        } else if (strcasecmp(tok, "uhbd") == 0) {
+            thee->writepotfmt = 2;
+        } else {
+            Vnm_print(2, "NOsh:  Invalid format (%s) while parsing WRITEPOT \
+keyword!\n", tok);
+            return -1;
+        }
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        strncpy(thee->writepotstem, tok, VMAX_ARGLEN);
+        thee->setwritepot = 1;
+        return 1;
+    } else if (strcasecmp(tok, "writeacc") == 0) {
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (sscanf(tok, "%d", &ti) == 0) {
+            Vnm_print(2, "NOsh:  Read non-float (%s) while parsing WRITEACC \
+keyword!\n", tok);
+            return -1;
+        }
+        thee->writeacc = ti;
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        if (strcasecmp(tok, "dx") == 0) {
+            thee->writeaccfmt = 0;
+        } else if (strcasecmp(tok, "avs") == 0) {
+            thee->writeaccfmt = 1;
+        } else if (strcasecmp(tok, "uhbd") == 0) {
+            thee->writeaccfmt = 2;
+        } else {
+            Vnm_print(2, "NOsh:  Invalid format (%s) while parsing \
+WRITEPOT keyword!\n", tok);
+            return -1;
+        }
+        VJMPERR1(Vio_scanf(sock, "%s", tok) == 1);
+        strncpy(thee->writeaccstem, tok, VMAX_ARGLEN);
+        thee->setwriteacc = 1;
+        return 1;
+    }
+
+    return 0;
+
+    VERROR1:
+        Vnm_print(2, "parsePBE:  ran out of tokens!\n");
+        return -1;
+
+}
