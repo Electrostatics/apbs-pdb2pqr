fd64caa6ef7f2ae1f33af98c7c6422a102820e43
Unknown <unknown@unknown>
2004-06-17 16:47:50 +0000

    Re-added license dir to fix CVS foul-up

---
+++ doc/license/LICENSE.c
@@ -0,0 +1,36 @@
+ *  @attention
+ *  @verbatim
+ *
+ * APBS -- Adaptive Poisson-Boltzmann Solver
+ *
+ * Nathan A. Baker (baker@biochem.wustl.edu)
+ * Dept. of Biochemistry and Molecular Biophysics
+ * Center for Computational Biology
+ * Washington University in St. Louis
+ *
+ * Additional contributing authors listed in the code documentation.
+ *
+ * Copyright (c) 2004.  Washington University in St. Louis.
+ * All Rights Reserved.
+ * Portions Copyright (c) 1999-2004.  The Regents of the University of
+ * California.  
+ * Portions Copyright (c) 1995.  Michael Holst.
+ *
+ * This file is part of APBS.
+ *
+ * APBS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * APBS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with APBS; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ * @endverbatim
+ */
