b6c38b3bcc65fc090b13d808f063762db8e162b3
todd_dolinsky <todd_dolinsky@a31b80ef-cfbe-4eed-bdb8-f46006999a5b>
2005-08-16 14:33:50 +0000

    TJD:  Updated license info on each Python/DAT class file.
    git-svn-id: https://svn.code.sf.net/p/pdb2pqr/code/trunk@122 a31b80ef-cfbe-4eed-bdb8-f46006999a5b

--- pdb2pqr/src/hydrogens.py
+++ pdb2pqr/src/hydrogens.py
@@ -5,11 +5,43 @@
     PDB2PQR. It uses a deterministic algorithm to perform flips, rotate
     alcoholic hydrogens, and optimize waters.
 
-    Based on C code from Jens Erik Nielsen
-    UCSD/HHMI
-    
-    Ported to Python by Todd Dolinsky (todd@ccb.wustl.edu)
+    ----------------------------
+   
+    PDB2PQR -- An automated pipeline for the setup, execution, and analysis of
+    Poisson-Boltzmann electrostatics calculations
+
+    Nathan A. Baker (baker@biochem.wustl.edu)
+    Todd Dolinsky (todd@ccb.wustl.edu)
+    Dept. of Biochemistry and Molecular Biophysics
+    Center for Computational Biology
     Washington University in St. Louis
+
+    Jens Nielsen (Jens.Nielsen@ucd.ie)
+    University College Dublin
+
+    Additional contributing authors listed in documentation and supporting
+    package licenses.
+
+    Copyright (c) 2003-2005.  Washington University in St. Louis.  
+    All Rights Reserved.
+
+    This file is part of PDB2PQR.
+
+    PDB2PQR is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+ 
+    PDB2PQR is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+   
+    You should have received a copy of the GNU General Public License
+    along with PDB2PQR; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+
+    ----------------------------
 """
 
 __date__ = "25 May 2005"
