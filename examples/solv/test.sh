60ac0a297c2510b03648756fca4ac939742f10f4
Unknown <unknown@unknown>
2005-11-14 16:30:40 +0000

    TJD:  Added +/- VSMALL to testing script.

--- examples/solv/test.sh
+++ examples/solv/test.sh
@@ -8,6 +8,7 @@ fi
 
 logfile=TESTRESULTS.log
 nettime=0
+vsmall=0.000000001000
 
 input=( apbs-mol apbs-smol apbs-spl2 ) 
 
@@ -37,39 +38,63 @@ do
  
   sync
 
+  # Methanol
+
+  fanswer=`printf "%.12f" ${answer[0]}`
+  fexpected=`printf "%.12f" ${methanol[i]}`
+  r=`echo "scale=12;if($fanswer>($fexpected-$vsmall) && $fanswer<($fexpected+$vsmall))r=1;if($fanswer == $fexpected)r=2;r" | bc`
   echo "Methanol Energy : ${answer[0]}"
-  if [[ ${answer[0]} == ${methanol[i]} ]]; then
-      echo "*** PASSED ***"
-      echo "           ${input[i]}.in (methanol): PASSED (${answer[0]})" >> $logfile
-  else
-      echo "*** FAILED ***"
-      echo "   APBS returned ${answer[0]}"
-      echo "   Expected result is ${methanol[i]}"
-      echo "           ${input[i]}.in (methanol): FAILED (${answer[0]}; expected ${methanol[i]})" >> $logfile
-  fi
+  case "$r" in
+      2) echo "*** PASSED ***"
+         echo "           ${input[i]}.in (methanol): PASSED (${answer[0]})" >> $logfile ;;
+      1)
+         echo "*** PASSED (with rounding error - see log) ***"
+         echo "           ${input[i]}.in (methanol): PASSED with rounding error (${answer[0]}; expected ${methanol[i]})" >> $logfile ;;
+      *)
+         echo "*** FAILED ***"
+         echo "   APBS returned ${answer[0]}"
+         echo "   Expected result is ${methanol[i]}"
+         echo "           ${input[i]}.in (methanol): FAILED (${answer[0]}; expected ${methanol[i]})" >> $logfile ;;
+  esac
 
+  # Methoxide
+
+  fanswer=`printf "%.12f" ${answer[1]}`
+  fexpected=`printf "%.12f" ${methoxide[i]}`
+  r=`echo "scale=12;if($fanswer>($fexpected-$vsmall) && $fanswer<($fexpected+$vsmall))r=1;if($fanswer == $fexpected)r=2;r" | bc`
   echo "Methoxide Energy: ${answer[1]}"
-  if [[ ${answer[1]} == ${methoxide[i]} ]]; then
-      echo "*** PASSED ***"
-      echo "           ${input[i]}.in (methoxide): PASSED (${answer[1]})" >> $logfile
-  else
-      echo "*** FAILED ***"
-      echo "   APBS returned ${answer[1]}"
-      echo "   Expected result is ${methoxide[i]}"
-      echo "           ${input[i]}.in (methoxide): FAILED (${answer[1]}; expected ${methoxide[i]})" >> $logfile
-  fi
+  case "$r" in
+      2) echo "*** PASSED ***"
+         echo "           ${input[i]}.in (methoxide): PASSED (${answer[1]})" >> $logfile ;;
+      1)
+         echo "*** PASSED (with rounding error - see log) ***"
+         echo "           ${input[i]}.in (methoxide): PASSED with rounding error (${answer[1]}; expected ${methoxide[i]})" >> $logfile ;;
+      *)
+         echo "*** FAILED ***"
+         echo "   APBS returned ${answer[1]}"
+         echo "   Expected result is ${methoxide[i]}"
+         echo "           ${input[i]}.in (methoxide): FAILED (${answer[1]}; expected ${methoxide[i]})" >> $logfile ;;
+  esac
 
+  # Difference
+
+  fanswer=`printf "%.12f" ${answer[2]}`
+  fexpected=`printf "%.12f" ${difference[i]}`
+  r=`echo "scale=12;if($fanswer>($fexpected-$vsmall) && $fanswer<($fexpected+$vsmall))r=1;if($fanswer == $fexpected)r=2;r" | bc`
   echo "Difference      : ${answer[2]}"
-  if [[ ${answer[2]} == ${difference[i]} ]]; then
-      echo "*** PASSED ***"
-      echo "           ${input[i]}.in (difference): PASSED (${answer[2]})" >> $logfile
-  else
-      echo "*** FAILED ***"
-      echo "   APBS returned ${answer[2]}"
-      echo "   Expected result is ${difference[i]}"
-      echo "           ${input[i]}.in (difference): FAILED (${answer[2]}; expected ${difference[i]})" >> $logfile
-  fi
-  
+  case "$r" in
+      2) echo "*** PASSED ***"
+         echo "           ${input[i]}.in (difference): PASSED (${answer[2]})" >> $logfile ;;
+      1)
+         echo "*** PASSED (with rounding error - see log) ***"
+         echo "           ${input[i]}.in (difference): PASSED with rounding error (${answer[2]}; expected ${difference[i]})" >> $logfile ;;
+      *)
+         echo "*** FAILED ***"
+         echo "   APBS returned ${answer[2]}"
+         echo "   Expected result is ${difference[i]}"
+         echo "           ${input[i]}.in (difference): FAILED (${answer[2]}; expected ${difference[i]})" >> $logfile ;;
+  esac
+ 
   endtime=`date +%s`
   let elapsed=$endtime-$starttime
   let nettime=$nettime+$elapsed
