e714e71ce267e67f900a1ef8e00a44fcce2da702
Unknown <unknown@unknown>
2005-12-07 21:18:41 +0000

    TJD:  Updated copyrights in code.

--- tools/python/vgrid/read.py
+++ tools/python/vgrid/read.py
@@ -16,7 +16,7 @@ header = "\n\n\
     Center for Computational Biology\n\
     Washington University in St. Louis\n\
     Additional contributing authors listed in the code documentation.\n\n\
-    Copyright (c) 2002-2004. Washington University in St. Louis\n\
+    Copyright (c) 2002-2005. Washington University in St. Louis\n\
     All Rights Reserved.\n\n\
     Portions copyright (c) 1999-2002.  University of California.\n\
     Portions copyright (c) 1995.  Michael Holst.\n\n\
