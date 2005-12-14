1bde7e36540fdcbd23e47f37713df1dd92cc56f3
Unknown <unknown@unknown>
2005-12-14 15:47:45 +0000

    TJD:  Added percent error to tests when tests fail.

--- examples/FKBP/test.sh
+++ examples/FKBP/test.sh
@@ -49,10 +49,12 @@ do
           echo "           1d7h-dmso/${input[i]}.in: PASSED (${answer[3]})" >> ../$logfile ;;
       1)  echo "*** PASSED (with rounding error - see log) ***"
           echo "           1d7h-dmso/${input[i]}.in: PASSED with rounding error (${answer[3]}; expected ${results[i]})" >> ../$logfile ;;
-      *)  echo "*** FAILED ***"
+      *)  error=`echo "scale=12;e=($fanswer - $fexpected)*100.0/$fexpected;;if(e<0)e=e*-1;e" | bc`
+          ferror=`printf "%.2f" $error`
+          echo "*** FAILED ***"
           echo "   APBS returned ${answer[3]}"
-          echo "   Expected result is ${results[i]}"
-          echo "           1d7h-dmso/${input[i]}.in: FAILED (${answer[3]}; expected ${results[i]})" >> ../$logfile ;;
+          echo "   Expected result is ${results[i]} ($ferror% error)"
+          echo "           1d7h-dmso/${input[i]}.in: FAILED (${answer[3]}; expected ${results[i]}; $ferror% error)" >> ../$logfile ;;
   esac
   
   endtime=`date +%s`
@@ -90,10 +92,12 @@ do
           echo "           1d7i-dss/${input[i]}.in: PASSED (${answer[3]})" >> ../$logfile ;;
       1)  echo "*** PASSED (with rounding error - see log) ***"
           echo "           1d7i-dss/${input[i]}.in: PASSED with rounding error (${answer[3]}; expected ${results[i]})" >> ../$logfile ;;
-      *)  echo "*** FAILED ***"
+      *)  error=`echo "scale=12;e=($fanswer - $fexpected)*100.0/$fexpected;;if(e<0)e=e*-1;e" | bc`
+          ferror=`printf "%.2f" $error`
+          echo "*** FAILED ***"
           echo "   APBS returned ${answer[3]}"
-          echo "   Expected result is ${results[i]}"
-          echo "           1d7i-dss/${input[i]}.in: FAILED (${answer[3]}; expected ${results[i]})" >> ../$logfile ;;
+          echo "   Expected result is ${results[i]} ($ferror% error)"
+          echo "           1d7i-dss/${input[i]}.in: FAILED (${answer[3]}; expected ${results[i]}; $ferror% error)" >> ../$logfile ;;
   esac
   
   endtime=`date +%s`
--- examples/hca-bind/test.sh
+++ examples/hca-bind/test.sh
@@ -45,10 +45,12 @@ do
           echo "           ${input[i]}.in: PASSED ($answer)" >> $logfile ;;
       1)  echo "*** PASSED (with rounding error - see log) ***"
           echo "           ${input[i]}.in: PASSED with rounding error ($answer; expected ${results[i]})" >> $logfile ;;
-      *)  echo "*** FAILED ***"
+      *)  error=`echo "scale=12;e=($fanswer - $fexpected)*100.0/$fexpected;;if(e<0)e=e*-1;e" | bc`
+          ferror=`printf "%.2f" $error`
+          echo "*** FAILED ***"
           echo "   APBS returned $answer"
-          echo "   Expected result is ${results[i]}"
-          echo "           ${input[i]}.in: FAILED ($answer; expected ${results[i]})" >> $logfile ;;
+          echo "   Expected result is ${results[i]} ($ferror% error)"
+          echo "           ${input[i]}.in: FAILED ($answer; expected ${results[i]}; $ferror% error)" >> $logfile ;;
   esac
   
   endtime=`date +%s`
