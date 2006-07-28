8f820983c4835af56dd70aeb8af38aef81ad5143
Unknown <unknown@unknown>
2006-07-28 13:23:12 +0000

    Accidentally overwrote the proper value.c file, uploading the original again. -D. Gohara

--- tools/mesh/value.c
+++ tools/mesh/value.c
@@ -16,7 +16,7 @@
  *
  * Additional contributing authors listed in the code documentation.
  *
- * Copyright (c) 2002-2006.  Washington University in St. Louis.
+ * Copyright (c) 2002-2005.  Washington University in St. Louis.
  * All Rights Reserved.
  * Portions Copyright (c) 1999-2002.  The Regents of the University of
  * California.  
@@ -37,29 +37,6 @@
  * You should have received a copy of the GNU General Public License
  * along with APBS; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
- *
- * Linking APBS statically or dynamically with other modules is making a
- * combined work based on APBS. Thus, the terms and conditions of the GNU
- * General Public License cover the whole combination.
- * 
- * SPECIAL GPL EXCEPTION
- * In addition, as a special exception, the copyright holders of APBS
- * give you permission to combine the APBS program with free software
- * programs and libraries that are released under the GNU LGPL or with
- * code included in releases of ISIM, Ion Simulator Interface, PMV, PyMOL
- * SMOL, VMD, and Vision. Such combined software may be linked with APBS and 
- * redistributed together in original or modified form as mere aggregation
- * without requirement that the entire work be under the scope of the GNU 
- * General Public License. This special exception permission is also extended
- * to any software listed in the SPECIAL GPL EXCEPTION clauses by the PMG,
- * FEtk, MC, or MALOC libraries.
- * 
- * Note that people who make modified versions of APBS are not obligated
- * to grant this special exception for their modified versions; it is
- * their choice whether to do so. The GNU General Public License gives
- * permission to release a modified version without this exception; this
- * exception also makes it possible to release a modified version which
- * carries forward this exception.
  *
  * @endverbatim
  */
@@ -93,10 +70,7 @@ int main(int argc, char **argv) {
     int inorm;
     char *path;
     double pt[3], val, grad[3];
-	
-	double x[81];
-	double y[5][81];
-	
+
     /* *************** CHECK INVOCATION ******************* */
     Vio_start();
     Vnm_redirect(1);
@@ -111,33 +85,37 @@ int main(int argc, char **argv) {
     path = argv[4];
 
     /* *************** READ DATA ******************* */
-	int i,k,s;
-	double j,m;
-	char *files[] = {"f1f94_para0.dx","f1f94_para1.dx","f1f94_para2.dx","f1f94_para3.dx"};
-	for(s=0;s<4;s++){
-		grid = Vgrid_ctor(0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, VNULL);
-		if (!Vgrid_readDX(grid, "FILE", "ASC", VNULL, files[s])) {
-			Vnm_print(2, "main:  Problem reading OpenDX-format grid from %s\n",
-					  path);
-			return 2;
-		}
-		double offset[] = {-30.45781,-15.01276,0.4322926,15.87734};
-		for(k=0,m=0;k<5;k++,m+=0.17385){
-			for(i=0,j=0.0;i<81;i++,j+=0.19029){
-				pt[0] = offset[s] + j;
-				pt[1] = -17.33536 + m;
-				pt[2] = -22.732;
-				Vgrid_value(grid, pt, &val);
-				x[i] = pt[0];
-				y[k][i] = val;
-			}
-		}
-		for(i=0;i<81;i++){
-			printf("%f %f %f %f %f %f\n",x[i],y[0][i],y[1][i],y[2][i],y[3][i],y[4][i]);
-		}
-	}
-	
+    Vnm_print(1, "Reading data from %s...\n", path);
+    grid = Vgrid_ctor(0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, VNULL);
+    if (!Vgrid_readDX(grid, "FILE", "ASC", VNULL, path)) {
+        Vnm_print(2, "main:  Problem reading OpenDX-format grid from %s\n",
+          path);
+        return 2;
+    }
 
+    /* *************** READ DATA ******************* */
+    Vnm_print(1, "\nData at (%g, %g, %g):\n", pt[0], pt[1], pt[2]);
+    if (Vgrid_value(grid, pt, &val)) {
+        Vnm_print(1, "Value = %1.12E kT/e\n", val);
+    } else  Vnm_print(1, "Unable to get value.\n");
+    if (Vgrid_gradient(grid, pt, grad)) {
+        Vnm_print(1, "Gradient = (%1.12E, %1.12E, %1.12E) kT/e/A\n", 
+          grad[0], grad[1], grad[2]);
+    } else  Vnm_print(1, "Unable to get gradient.\n");
+    if (Vgrid_curvature(grid, pt, 0, &val)) {
+        Vnm_print(1, "Reduced maximal curvature = %1.12E kT/e/A/A\n", val);
+    } else Vnm_print(1, "Unable to get curvature.\n");
+    if (Vgrid_curvature(grid, pt, 1, &val)) {
+        Vnm_print(1, "Mean curvature (Laplace) = %1.12E kT/e/A/A\n", val);
+    } else Vnm_print(1, "Unable to get curvature.\n");
+    if (Vgrid_curvature(grid, pt, 2, &val)) {
+        Vnm_print(1, "Gauss curvature = %1.12E kT/e/A/A\n", val);
+    } else Vnm_print(1, "Unable to get curvature.\n");
+    if (Vgrid_curvature(grid, pt, 3, &val)) {
+        Vnm_print(1, "True maximal curvature = %1.12E kT/e/A/A\n", val);
+    } else Vnm_print(1, "Unable to get curvature.\n");
+
+    Vnm_print(1, "\n");
     return 0;
 
 }
