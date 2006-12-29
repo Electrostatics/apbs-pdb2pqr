df73fa9f5c77dc3602eeece17e07e08521e0854c
Nathan Baker <Nathan.Baker@pnnl.gov>
2006-12-29 22:01:25 +0000

    Added new "alkanes" example to show off APOLAR functionality.

--- examples/point-pmf/runme.sh
+++ examples/point-pmf/runme.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# Put the APBS executeable dir in bindir, i.e:
-#bindir="../../../dist/bin/i686-pc-linux/"
-bindir=''
-
-if [ "$bindir" = '' ]; then
-    echo "Please edit runme.sh to add your APBS executeable directory to the path."
-    exit
+apbspath=`which apbs`
+if test -z "${apbspath}"; then
+	echo ""
+	echo "Please add the apbs binary to your path..."
+	echo ""
+	exit
 fi
-
 
 coord='-2.000 -1.00 0.000 1.000 2.000'
 
+echo "One charge is fixed at x = -3.00..."
 for i in $coord; do
-  echo "Moving ion to $i..."
-# TO TRANSLATE IN THE X DIRECTION:
-  echo "ATOM      1  I   ION     1      -3.000   0.000   0.000  1.00  0.00" \
-     > mol1.pqr
-  echo "ATOM      1  I   ION     1      $i    0.000   0.000  1.00 0.00"\
-     > mol2.pqr
+  # TO TRANSLATE IN THE X DIRECTION:
+  echo "ATOM      1  I   ION     1      -3.000   0.000   0.000  1.00  0.00" > mol1.pqr
+  echo "ATOM      1  I   ION     1      $i    0.000   0.000  1.00 0.00" > mol2.pqr
   cat mol1.pqr mol2.pqr > complex.pqr
-  ${bindir}/apbs apbs.in > OUTPUT_${i} 2>&1
+  outfile=OUTPUT_${i}
+  apbs apbs.in > ${outfile}
+  echo "x = ${i}"
+  grep "Global" ${outfile}
 done
