e8b25dd00b34b792f857db3c01713400e5b04f0e
Unknown <unknown@unknown>
2007-07-18 15:30:23 +0000

    Fixed duplicate definitions of routines when the --with-tinker flag is used. Fixed a bug introduced in apolparm.h line 189 regarding the setwat flag.

--- src/generic/vatom.c
+++ src/generic/vatom.c
@@ -280,35 +280,4 @@ VPUBLIC double *Vatom_getNLInducedDipole(Vatom *thee) {
 
 }
 
-VPUBLIC void Vatom_setResName(Vatom *thee, char resName[VMAX_ARGLEN]) { 
-	
-	VASSERT(thee != VNULL);
-	strcpy(thee->resName, resName);
-	
-}
-
-VPUBLIC void Vatom_getResName(Vatom *thee, char resName[VMAX_ARGLEN]) { 
-	
-	
-	VASSERT(thee != VNULL);
-	strcpy(resName,thee->resName);
-	return 1; 
-	
-}
-
-VPUBLIC void Vatom_setAtomName(Vatom *thee, char atomName[VMAX_ARGLEN]) { 
-	
-	VASSERT(thee != VNULL);
-	strcpy(thee->atomName, atomName);
-	
-}
-
-VPUBLIC void Vatom_getAtomName(Vatom *thee, char atomName[VMAX_ARGLEN]) { 
-	
-	VASSERT(thee != VNULL);
-	strcpy(atomName,thee->atomName);
-	return 1; 
-	
-}
-
 #endif /* if defined(WITH_TINKER) */
