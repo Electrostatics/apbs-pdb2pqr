587b19720a4fc77cb8a8fd9ca8385fb2244e8627
yhuang01 <yhuang01@a31b80ef-cfbe-4eed-bdb8-f46006999a5b>
2007-11-06 17:10:56 +0000

    License updates from GPL to BSD.
    git-svn-id: https://svn.code.sf.net/p/pdb2pqr/code/trunk@517 a31b80ef-cfbe-4eed-bdb8-f46006999a5b

--- pdb2pqr/propka/propkac.c
+++ pdb2pqr/propka/propkac.c
@@ -5,47 +5,35 @@
     PDB2PQR -- An automated pipeline for the setup, execution, and analysis of
     Poisson-Boltzmann electrostatics calculations
 
-    Nathan A. Baker (baker@biochem.wustl.edu)
-    Todd Dolinsky (todd@ccb.wustl.edu)
-    Dept. of Biochemistry and Molecular Biophysics
-    Center for Computational Biology
-    Washington University in St. Louis
+	Copyright (c) 2002-2007, Jens Erik Nielsen, University College Dublin; 
+	Nathan A. Baker, Washington University in St. Louis; Paul Czodrowski & 
+	Gerhard Klebe, University of Marburg
 
-    Jens Nielsen (Jens.Nielsen@ucd.ie)
-    University College Dublin
+	All rights reserved.
 
-    Additional contributing authors listed in documentation and supporting
-    package licenses.
+	Redistribution and use in source and binary forms, with or without modification, 
+	are permitted provided that the following conditions are met:
 
-    Copyright (c) 2003-2005.  Washington University in St. Louis.  
-    All Rights Reserved.
+		* Redistributions of source code must retain the above copyright notice, 
+		  this list of conditions and the following disclaimer.
+		* Redistributions in binary form must reproduce the above copyright notice, 
+		  this list of conditions and the following disclaimer in the documentation 
+		  and/or other materials provided with the distribution.
+		* Neither the names of University College Dublin, Washington University in 
+		  St. Louis, or University of Marburg nor the names of its contributors may 
+		  be used to endorse or promote products derived from this software without 
+		  specific prior written permission.
 
-    This file is part of PDB2PQR.
-
-    Redistribution and use in source and binary forms, with or without 
-    modification, are permitted provided that the following conditions 
-    are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the University of Copenhagen nor the names of its
-      contributors may be used to endorse or promote products derived from 
-      this software without specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
-    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+	IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+	INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+	OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+	OF THE POSSIBILITY OF SUCH DAMAGE.
 
     ----------------------------
 */
