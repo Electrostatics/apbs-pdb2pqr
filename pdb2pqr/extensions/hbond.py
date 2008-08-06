f238046aea9d1fdf466d014df67fb8a0e5c6704a
yhuang01 <yhuang01@a31b80ef-cfbe-4eed-bdb8-f46006999a5b>
2008-08-06 17:56:48 +0000

    Updated hbond.py with Mike Bradley's definitions by default, printing out warning messages at run time.
    git-svn-id: https://svn.code.sf.net/p/pdb2pqr/code/trunk@707 a31b80ef-cfbe-4eed-bdb8-f46006999a5b

--- pdb2pqr/extensions/hbond.py
+++ pdb2pqr/extensions/hbond.py
@@ -3,19 +3,33 @@
 
     Find all hydrogen bonds as determined by the cutoffs below.
     Uses PDB2PQR to determine donors and acceptors, and displays
-    all available bonds to stdout.
+    all available bonds to stdout.  Please read the "NOTE" comments
+    within the code for additional important information.
 
-    Author:  Todd Dolinsky
+    Authors:  Todd Dolinsky, Michael J Bradley
 """
 
 __date__ = "17 February 2006"
 __author__ = "Todd Dolinsky"
+# NOTE: This extension edited and updated on 05 August 2008 by Michael J Bradley
+# NOTE: The definitions for hbonds used below were utilized in the following study:
+# Bradley MJ, Chivers PT, Baker NA. Molecular dynamics simulation of the Escherichia coli NikR protein: Equilibrium conformational fluctuations reveal inter-domain allosteric communication pathways. Journal of Molecular Biology, 378, 1155-1173, 2008.  http://dx.doi.org/10.1016/j.jmb.2008.03.010
+
+import sys
 
 from src.utilities import *
 from src.routines import *
 
-ANGLE_CUTOFF = 20.0       # A - D - H(D) angle
-DIST_CUTOFF = 3.3         # H(D) to A distance
+# NOTE: You should try different angle cutoffs and see how they affect your results
+ANGLE_CUTOFF = 30.0       # A - D - H(D) angle
+#ANGLE_CUTOFF = 20.0       # A - D - H(D) angle
+
+# NOTE: This hbond extension uses a different distance definition (not the H(D) - A distance).  You should still try different values for this distance cutoff and see how it affects your results.
+DIST_CUTOFF = 3.4         # D to A distance
+
+# NOTE: If you wish to use the previous hbond.py defintiion, comment out "DIST_CUTOFF" above, uncomment the next line below, and change "ANGLE_CUTOFF" to 20.0
+#DIST_CUTOFF = 3.3         # H(D) to A distance
+
 
 def usage():
     str  = "        --hbond       :  Print a list of hydrogen bonds to\n"
@@ -34,6 +48,10 @@ def hbond(routines, outroot):
     file = open(outname, "w")
 
     routines.write("Printing hydrogen bond list...\n")
+
+    sys.stderr.write("Warning: New H-bonding definition is being used- see hbond.py in extensions directory for details!\n")
+
+    file.write("# Warning: New H-bonding definition is being used- see hbond.py in extensions directory for details!\n")
 
     # Initialize - set nearby cells, donors/acceptors
     # The cell size adds one for the D-H(D) bond, and rounds up
@@ -64,13 +82,15 @@ def hbond(routines, outroot):
 
                 # Do distance and angle checks
                 
-                dist = distance(donorh.getCoords(), acc.getCoords())
+                #NOTE: If you wish to use the previous hbond.py definition, toggle the "dist" variable comments below
+				#dist = distance(donorh.getCoords(), acc.getCoords())
+                dist = distance(donor.getCoords(), acc.getCoords())
                 if dist > DIST_CUTOFF: continue
                 angle = getAngle(acc.getCoords(), donor.getCoords(), donorh.getCoords())
                 if angle > ANGLE_CUTOFF: continue
-                routines.write("Donor: %s %s\tAcceptor: %s %s\tHdist: %.2f\tAngle: %.2f\n" % \
+                routines.write("Donor: %s %s\tAcceptor: %s %s\tdist: %.2f\tAngle: %.2f\n" % \
                       (donor.residue, donor.name, acc.residue, acc.name, dist, angle)) 
-                file.write("Donor: %s %s\tAcceptor: %s %s\tHdist: %.2f\tAngle: %.2f\n" % \
+                file.write("Donor: %s %s\tAcceptor: %s %s\tdist: %.2f\tAngle: %.2f\n" % \
                       (donor.residue, donor.name, acc.residue, acc.name, dist, angle))
 
     routines.write("\n")
