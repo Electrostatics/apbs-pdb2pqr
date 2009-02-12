b969fedeefcf5d86c79a1f0c1f8135a6d9f51f7d
Unknown <unknown@unknown>
2009-02-12 01:26:03 +0000

    Updated version number.

--- tools/python/vgrid/average.py
+++ tools/python/vgrid/average.py
@@ -11,7 +11,7 @@ from sys import stdout, stderr
 header = "\n\n\
     ----------------------------------------------------------------------\n\
     Adaptive Poisson-Boltzmann Solver (APBS)\n\
-    Version 1.0.0 (April 2008)\n\
+    Version 1.1.0 (February 2009)\n\
     \n\
     Nathan A. Baker (baker@biochem.wustl.edu)\n\
     Dept. of Biochemistry and Molecular Biophysics\n\
