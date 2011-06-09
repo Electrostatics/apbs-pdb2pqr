5f1b773ce33624fa31b7268ef729e30e343e690a
mabaran <mabaran@a31b80ef-cfbe-4eed-bdb8-f46006999a5b>
2011-06-09 16:51:07 +0000

    Almost working version
    git-svn-id: https://svn.code.sf.net/p/pdb2pqr/code/trunk@960 a31b80ef-cfbe-4eed-bdb8-f46006999a5b

--- pdb2pqr/conf_avg/Andromeda.py
+++ pdb2pqr/conf_avg/Andromeda.py
@@ -1,22 +1,54 @@
-apbs_inputfile=self.apbs_setup.printInput()
-self.APBS=runAPBS()
-potentials = self.APBS.runAPBS(self.protein, apbs_inputfile, CM)
-self.APBS.cleanup()
+#!/usr/bin/env python
+print 'Hello'
+import sys
+sys.path.append('/home/mag_local/development/pdb2pqr')
+import string
+import math
+import string
+import getopt
+import time
+from src import pdb
+from src import utilities
+from src import structures
+from src import routines
+from src import protein
+from src import server
+from src.pdb import *
+from src.utilities import *
+from src.structures import *
+from src.definitions import *
+from src.forcefield import *  
+from src.routines import *
+from src.protein import *
+from src.server import *
+from StringIO import *
+from src.hydrogens import *
+
+#
+# Read the pdb file
+#
+verbose=True
+ff='parse'
+myDefinition = Definition()
+#
+pdbfile_name='2lzt.pka.pdb'
+pdbfile = getPDBFile(pdbfile_name)
+pdblist, errlist = readPDB(pdbfile)
                
 
 # Here, self.protein is the PDB2PQR instance. you will get this with something like:
-dummydef = Definition()
-myProtein = Protein(pdblist, dummydef)
+myProtein = Protein(pdblist, myDefinition)
 
 # then you do:
-myRoutines = Routines(myProtein, verbose) #myDefinition)
+myRoutines = Routines(myProtein, verbose)
 myRoutines.updateResidueTypes()
 myRoutines.updateSSbridges()
 myRoutines.updateBonds()
 myRoutines.setTermini()
 myRoutines.updateInternalBonds()
 
-myRoutines.applyNameScheme(Forcefield(ff, myDefinition, None))
+myforcefield=Forcefield(ff, myDefinition, None)
+myRoutines.applyNameScheme(myforcefield)
 myRoutines.findMissingHeavy()
 myRoutines.addHydrogens()
 myRoutines.debumpProtein()
@@ -32,7 +64,7 @@ myRoutines.updateInternalBonds()
 myRoutines.calculateDihedralAngles()
 myhydRoutines = hydrogenRoutines(myRoutines)
 #
-# Here we should inject the info!!
+# Now optimize hydrogens
 #
 myhydRoutines.setOptimizeableHydrogens()
 myhydRoutines.initializeFullOptimization()
@@ -40,12 +72,37 @@ myhydRoutines.optimizeHydrogens()
 myhydRoutines.cleanup()
 myRoutines.setStates()
 
-# Choose the force field:
-myForcefield = Forcefield(ff, myDefinition, None)
+print "Created protein object (after processing myRoutines) -"
+print "\tNumber of residues in protein: %s" % myProtein.numResidues()
+print "\tNumber of atoms in protein   : %s" % myProtein.numAtoms()
 
 # To print the charges do:
-for chain in protein.getChains():
-    for residue in chain.get("residues"):
-        for atom in residue.get("atoms"):
-            atomname = atom.get("name")
-            charge, radius = forcefield.getParams(residue, atomname)
+if False:
+    for chain in myProtein.getChains():
+        print 'Chain',chain
+        for residue in chain.get("residues"):
+            print residue
+            for atom in residue.get("atoms"):
+                atomname = atom.get("name")
+                charge, radius = myforcefield.getParams1(residue, atomname)
+                print atomname,charge,radius
+
+#import src.psize
+#size=src.psize.Psize()
+
+method=""
+async=0
+split=0
+import pdb2pka.inputgen_pKa as IP
+igen = IP.inputGen(pdbfile_name)
+igen.maps=0
+igen.set_type('desolv')
+igen.pdie=8
+
+apbs_inputfile=igen.printInput()
+import pdb2pka.apbs as RAPBS
+APBS=RAPBS.runAPBS()
+potentials = APBS.runAPBS(myProtein, apbs_inputfile)
+APBS.cleanup()
+
+print potentials
