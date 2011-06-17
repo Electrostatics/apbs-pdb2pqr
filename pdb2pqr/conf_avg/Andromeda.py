5d7232e8bfb35a0d73244f5201bfda4492bbd9f2
mabaran <mabaran@a31b80ef-cfbe-4eed-bdb8-f46006999a5b>
2011-06-17 18:05:12 +0000

    everything is in a class; has option for running 2 files
    git-svn-id: https://svn.code.sf.net/p/pdb2pqr/code/trunk@965 a31b80ef-cfbe-4eed-bdb8-f46006999a5b

--- pdb2pqr/conf_avg/Andromeda.py
+++ pdb2pqr/conf_avg/Andromeda.py
@@ -41,96 +41,135 @@ from src.server import *
 from StringIO import *
 from src.hydrogens import *
 
-#
-# Read the pdb file
-#
-verbose=True
-ff='parse'
+class conf_avg:
 
-#
-# Read the PDB file
-#
-pdbfile_name='2lzt.pka.pdb'
-pdbfile = getPDBFile(pdbfile_name)
-pdblist, errlist = readPDB(pdbfile)
-               
-#
-# Instantiate pdb2pqr
-#
-myDefinition = Definition()
-myProtein = Protein(pdblist, myDefinition)
+	def __init__(self,options):
+		
+		potentials=[]
 
-#
-# Setup everything
-#
-myRoutines = Routines(myProtein, verbose)
-myRoutines.updateResidueTypes()
-myRoutines.updateSSbridges()
-myRoutines.updateBonds()
-myRoutines.setTermini()
-myRoutines.updateInternalBonds()
+		for currentPDB in options.pdbfile_name:
+			pdbfile = getPDBFile(currentPDB)
+			pdblist, errlist = readPDB(pdbfile)
 
-myforcefield=Forcefield(ff, myDefinition, None)
-myRoutines.applyNameScheme(myforcefield)
+			pots=self.get_potentials(currentPDB,pdblist)
+			potentials.append(pots)
+		#
+		# Average potentials
+		#
+		avg_pots=self.average_potentials(potentials)
+		return
 
-myRoutines.findMissingHeavy()
-myRoutines.addHydrogens()
-myRoutines.debumpProtein()
-myProtein.reSerialize()
+	def get_potentials(self,currentPDB,pdblist):
+		"""Get the potentials by first running pdb2pqr and then apbs"""
+		myProtein,apbs_inputfile=self.run_pdb2pqr(currentPDB,pdblist)
+		potentials=self.run_apbs(myProtein,apbs_inputfile)
+		return potentials
+	
+	def run_pdb2pqr(self,currentPDB,pdblist):
+        """Run pdb2pqr, prepare input for apbs"""
+		#
+		# Instantiate pdb2pqr
+		#
+		myDefinition = Definition()
+		myProtein = Protein(pdblist, myDefinition)
 
-#
-# Addn and optimze hydrogens:
-# 
-from src.hydrogens import hydrogenRoutines
-myRoutines.updateInternalBonds()
-myRoutines.calculateDihedralAngles()
-myhydRoutines = hydrogenRoutines(myRoutines)
-#
-# Now optimize hydrogens
-#
-myhydRoutines.setOptimizeableHydrogens()
-myhydRoutines.initializeFullOptimization()
-myhydRoutines.optimizeHydrogens()
-myhydRoutines.cleanup()
-myRoutines.setStates()
+		#
+		# Setup everything
+		#
+		myRoutines = Routines(myProtein, verbose)
+		myRoutines.updateResidueTypes()
+		myRoutines.updateSSbridges()
+		myRoutines.updateBonds()
+		myRoutines.setTermini()
+		myRoutines.updateInternalBonds()
+	
+		myforcefield=Forcefield(ff, myDefinition, None)
+		myRoutines.applyNameScheme(myforcefield)
+	
+		myRoutines.findMissingHeavy()
+		myRoutines.addHydrogens()
+		myRoutines.debumpProtein()
+		myProtein.reSerialize()
+	
+		#
+		# Add and optimze hydrogens:
+		# 
+		from src.hydrogens import hydrogenRoutines
+		myRoutines.updateInternalBonds()
+		myRoutines.calculateDihedralAngles()
+		myhydRoutines = hydrogenRoutines(myRoutines)
+		#
+		# Now optimize hydrogens
+		#
+		myhydRoutines.setOptimizeableHydrogens()
+		myhydRoutines.initializeFullOptimization()
+		myhydRoutines.optimizeHydrogens()
+		myhydRoutines.cleanup()
+		myRoutines.setStates()
+	
+		print "Created protein object (after processing myRoutines) -"
+		print "\tNumber of residues in protein: %s" % myProtein.numResidues()
+		print "\tNumber of atoms in protein   : %s" % myProtein.numAtoms()
+		
+		#
+		# Assign charges
+		#
+		for chain in myProtein.getChains():
+		    for residue in chain.get("residues"):
+		        for atom in residue.get("atoms"):
+		            atomname = atom.get("name")
+		            charge, radius = myforcefield.getParams1(residue, atomname)
+		            atom.set("radius", radius)
+		            atom.set("ffcharge", charge)
+		
+		#import src.psize
+		#size=src.psize.Psize()
+	
+		method=""
+		async=0
+		split=0
+		import pdb2pka.inputgen_pKa as IP
+		igen = IP.inputGen(currentPDB)
+		igen.maps=None
+		igen.set_type('intene')
+		igen.pdie=8.0
+		igen.sdie=80.0
+		all_center,extent=igen.getCenter()
+		igen.setfineCenter(all_center)
+		print 'Center: %5.1fA %5.1fA %5.1fA' %(all_center[0],all_center[1],all_center[2])
+		print 'Extent: %5.1fA %5.1fA %5.1fA'  %(extent[0],extent[1],extent[2])
 
-print "Created protein object (after processing myRoutines) -"
-print "\tNumber of residues in protein: %s" % myProtein.numResidues()
-print "\tNumber of atoms in protein   : %s" % myProtein.numAtoms()
+		apbs_inputfile=igen.printInput()
 
-#
-# Assign charges
-#
-for chain in myProtein.getChains():
-    for residue in chain.get("residues"):
-        for atom in residue.get("atoms"):
-            atomname = atom.get("name")
-            charge, radius = myforcefield.getParams1(residue, atomname)
-            atom.set("radius", radius)
-            atom.set("ffcharge", charge)
+		return myProtein, apbs_inputfile
 
-#import src.psize
-#size=src.psize.Psize()
+	def run_apbs(self,myProtein,apbs_inputfile):
+		"""runs apbs"""
+		from pdb2pka.apbs import *
+		APBS=runAPBS()
+		potentials = APBS.runAPBS(myProtein, apbs_inputfile)
+		APBS.cleanup()
 
-method=""
-async=0
-split=0
-import pdb2pka.inputgen_pKa as IP
-igen = IP.inputGen(pdbfile_name)
-igen.maps=None
-igen.set_type('intene')
-igen.pdie=8.0
-igen.sdie=80.0
-all_center,extent=igen.getCenter()
-igen.setfineCenter(all_center)
-print 'Center: %5.1fA %5.1fA %5.1fA' %(all_center[0],all_center[1],all_center[2])
-print 'Extent: %5.1fA %5.1fA %5.1fA'  %(extent[0],extent[1],extent[2])
+		return potentials
+
+	def average_potentials(self,potentials):
+		"""This function will average many potential maps"""
+
+		avg_pots=potentials
+
+		return avg_pots
 
 
-apbs_inputfile=igen.printInput()
-from pdb2pka.apbs import *
-APBS=runAPBS()
-potentials = APBS.runAPBS(myProtein, apbs_inputfile)
-APBS.cleanup()
+if __name__=='__main__':
+	from optparse import OptionParser
+	parser = OptionParser(usage='%prog [options]',version='%prog 1.0')
+	parser.add_option('-p','--pdb',dest='pdbfile_name',action='store',type='string',nargs=2,default='2lzt.pka.pdb',
+                  help='The PDB file. Default: %default')
 
-print potentials
+	(options, args) = parser.parse_args()
+
+	verbose=True
+	ff='parse'
+
+	I=conf_avg(options)
+
