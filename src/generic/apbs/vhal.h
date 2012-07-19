87af112365ad70d3266f9186ceba63ce5af96a54
Nathan Baker <Nathan.Baker@pnnl.gov>
2012-07-19 00:53:54 +0000

    Added WIN32-specific macro to vhal.h for compatibility

--- src/generic/apbs/vhal.h
+++ src/generic/apbs/vhal.h
@@ -15,20 +15,12 @@
  *
  * APBS -- Adaptive Poisson-Boltzmann Solver
  *
- *  Nathan A. Baker (nathan.baker@pnnl.gov)
- *  Pacific Northwest National Laboratory
+ * Nathan A. Baker (nathan.baker@pnl.gov)
+ * Pacific Northwest National Laboratory
  *
- *  Additional contributing authors listed in the code documentation.
+ * Additional contributing authors listed in the code documentation.
  *
- * Copyright (c) 2010-2011 Battelle Memorial Institute. Developed at the 
- * Pacific Northwest National Laboratory, operated by Battelle Memorial 
- * Institute, Pacific Northwest Division for the U.S. Department of Energy.
- *
- * Portions Copyright (c) 2002-2010, Washington University in St. Louis.
- * Portions Copyright (c) 2002-2010, Nathan A. Baker.
- * Portions Copyright (c) 1999-2002, The Regents of the University of 
- * California.
- * Portions Copyright (c) 1995, Michael Holst.
+ * Copyright (c) 2010-2011 Battelle Memorial Institute. Developed at the Pacific Northwest National Laboratory, operated by Battelle Memorial Institute, Pacific Northwest Division for the U.S. Department Energy.  Portions Copyright (c) 2002-2010, Washington University in St. Louis.  Portions Copyright (c) 2002-2010, Nathan A. Baker.  Portions Copyright (c) 1999-2002, The Regents of the University of California. Portions Copyright (c) 1995, Michael Holst.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -56,22 +48,6 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Neither the name of the developer nor the names of its contributors may be 
- * used to endorse or promote products derived from this software without 
- * specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
- * THE POSSIBILITY OF SUCH DAMAGE.
  *
  * @endverbatim
  */
@@ -680,6 +656,9 @@ typedef enum eVdata_Format Vdata_Format;
  *  @ingroup Vhal
  *  @note    The do{...} while(0) simply enforces that a semicolon at the end
  */
+#if WIN32
+#	define snprintf sprintf_s
+#endif 
 #define VASSERT_MSG2(cnd, msg, arg0, arg1)                           \
 	do {                                                             \
 		if( (cnd) == 0 ) {                                           \
