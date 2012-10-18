d48f84d9f8c81243a22d2215c6b9787ca27af7a0
dusktreader <tucker.beck@gmail.com>
2012-10-17 18:52:26 -0700

    Preliminary combination build of apbs with internal maloc library

--- contrib/maloc/build/cleanup.sh
+++ contrib/maloc/build/cleanup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 rm -rf CMakeFiles
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
+rm CMakeCache.txt -f
+rm cmake_install.cmake -f
+rm Makefile -f
 rm -rf src
-
+rm *~ -f
+rm install_manifest.txt -f
+rm maloc.cbp -f
