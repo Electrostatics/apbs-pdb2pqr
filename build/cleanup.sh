a92ef1f7dd815ca565f81d4dffb6fe44180714cd
dusktreader <tucker.beck@gmail.com>
2012-10-24 12:59:53 -0700

    Dealt with documentation.

--- build/cleanup.sh
+++ build/cleanup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-rm -rf ../bin/* ../tools/bin/* CMakeFiles CMakeCache.txt cmake_install.cmake Makefile src install_manifest.txt apbs.cbp maloc-prefix tools fontconfig *~
+rm -rf ../bin/* ../tools/bin/* CMakeFiles CMakeCache.txt cmake_install.cmake Makefile src install_manifest.txt apbs.cbp maloc-prefix tools fontconfig ../tests/*.pyc ../tests/io.mc ../tests/*~ ../tests/test.log doc ../doc/programmer/html
+
+find .. -name *~ | xargs rm
 
