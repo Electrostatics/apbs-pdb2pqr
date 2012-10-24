626d91d0ff7c853c1c86e664e13dacff5bd82cea
jens_nielsen <jens_nielsen@a31b80ef-cfbe-4eed-bdb8-f46006999a5b>
2012-10-24 11:32:58 +0000

    temporary hack fix for missing errors module. Should be replaced with the real module
    git-svn-id: https://svn.code.sf.net/p/pdb2pqr/code/trunk@1072 a31b80ef-cfbe-4eed-bdb8-f46006999a5b

---
+++ pdb2pqr/src/errors.py
@@ -0,0 +1,18 @@
+
+class PDBInternalError:
+    #
+    # Dummy class to get pdb2pqr to work
+    #
+    
+    def __init__(self):
+        return
+    
+class PDBInputError:
+
+    def __init__(self):
+        return
+
+class PDB2PQRError:
+
+    def __init__(self):
+        return
