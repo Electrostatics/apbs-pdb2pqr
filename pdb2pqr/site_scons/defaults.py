967fa4c632b1fcbc3ebf0666b56d2f9fe6d3f915
Kyle Monson <kyle.monson@pnnl.gov>
2014-12-02 11:40:01 -0800

    Fixed version in master.

--- pdb2pqr/site_scons/defaults.py
+++ pdb2pqr/site_scons/defaults.py
@@ -12,4 +12,4 @@ codePath = os.getcwd()
 pythonBin = sys.executable
 
 buildTime = datetime.today()
-productVersion = 'monson-dev BRANCH'# + buildTime.strftime('%Y%m%d%H%M')
+productVersion = 'master'# + buildTime.strftime('%Y%m%d%H%M')
