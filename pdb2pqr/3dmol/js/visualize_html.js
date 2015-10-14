04f0a6a7258d4a6f253a715696211b4efffe967f
karinaliles <lileskr@email.sc.edu>
2015-10-14 16:34:12 -0400

    updated 3dmol ui

--- pdb2pqr/3dmol/js/visualize_html.js
+++ pdb2pqr/3dmol/js/visualize_html.js
@@ -11,55 +11,57 @@ function build_page(jobid){
 	
 	//change surface
     var b = 
-    "<table border='0'><tr><td width='25%' valign='top'><font style='color:white; font-size:12pt'>Surface: </font></td><td colspan='2'> &nbsp;<font style='color: black; font-size: 8pt; font-style: italic'>Click to change</font><br>" +
-"    <select class='styled-select' id='selected_surface' onchange='update_surface(1)' >" +
+    "<br><font style='color:white; font-size:12pt'><center>Surface</font><hr width='75%'>" +
+"    <select class='styled-select' id='selected_surface' onchange='update_surface(1)' style='max-width:50%;'>" +
 "       <option style='color: black;' value='SAS'>Solvent Accessible</option>" +
 "<option style='color: black;' value='SES'>Solvent Excluded</option>" +
 "<option style='color: black;' value='VDW'>Van Der Waals</option>" +
-"</select> <br><br>" +
+"</select><br><br>" +
 
-"<select class='styled-select' id='selected_vis' onchange='set_vis()'>"+
+"<div class='inner'><ul class='button-group round' class='leftbutton'><input type='button' button class='button-backbone pure-button' style='width: 85px; height: 30px; color: black' value='On' onclick='on_surface()'></button></input>" +
+
+"    <input type='button' button class='button-backbone pure-button' style='width: 85px; height: 30px; color: black' input type='button' value='Off' onclick='glviewer.removeSurface(surf)'></button></input></ul></div>" +
+
+"<div class='inner'><ul class='button-group round' class='rightbutton'><input type='button' button class='button-labela pure-button' style='width: 85px; height: 30px; color: black; font-size: 10pt' value='Translucent' onclick='update_surface(2)'></button></input>" +
+
+"    <input type='button' button class='button-unlabela pure-button' style='width: 85px; height: 30px; color: black' input type='button' value='Opaque' onclick='update_surface(3)'></button></input></ul></div>" +
+
+"<font style='color:white; font-size:12pt'>Isosurface</font><hr width='75%'>" +
+
+//change min isoval
+" <table class='main' width='70%' border='0'><tr><td style='color: white; font-size: 12pt'><br>Min: </td> " +
+"<td><br><input type='text' id='min_isoval2' value='-5' style='text-align:right; width: 50px; height: 30px; background: transparent; border:0; color: white; font-size: 18px'> </td>" +
+"<td style='color: white; font-size: 18px'><br>kT/e &nbsp;&nbsp;</td>" +
+"<td valign='top'><br><br><input type=range min=-50 max=50 value=-5 id='min_isoval2' step=1 onmouseup='set_min_isoval2(value)'> </td></tr></table>" +
+
+//change max isoval
+" <table class='main' width='70%' border='0'><tr><td style='color: white; font-size: 12pt'>Max: </td> " +
+"<td><input type='text' id='max_isoval2' value='5' style='text-align:right; width: 50px; height: 30px; background: transparent; border:0; color: white; font-size: 18px'> </td>" +
+"<td style='color: white; font-size: 18px'>kT/e &nbsp;&nbsp;</td>" +
+"<td valign='top' ><br><input type=range min=-50 max=50 value=5 id='max_isoval2' step=1 onmouseup='set_max_isoval2(value)'> </td></tr></table>" +
+
+"<font style='color:white; font-size:12pt'>Model</font><hr width='75%'>" +
+
+"<select class='styled-select' id='selected_vis' onchange='set_vis()' style='max-width:50%;'>"+
 "        <option style='color: black;' value='line'>Line </option> "+
 "        <option style='color: black;' value='stick'>Stick </option>" +
 "        <option style='color: black;' value='cross'>Cross </option>"+
 "        <option style='color: black;' value='sphere'>Sphere </option>"+
 "        <option style='color: black;' value='cartoon'>Cartoon </option>"+
-"    </select></td></tr>" 
-+ "<tr><td>&nbsp;</td><td height='45'><div class='inner'><ul class='button-group round' class='leftbutton'><input type='button' button class='button-backbone pure-button' style='width: 125px; height: 40px; color: black' value='On' onclick='on_surface()'></button></input>" +
-"    <input type='button' button class='button-backbone pure-button' style='width: 125px; height: 40px; color: black' input type='button' value='Off' onclick='glviewer.removeSurface(surf)'></button></input></ul></div></td></tr>" +
-"<tr><td>&nbsp;</td><td height='45'><div class='inner'><ul class='button-group round' class='rightbutton'><input type='button' button class='button-labela pure-button' style='width: 125px; height: 40px; color: black; font-size: 10pt' value='Translucent' onclick='update_surface(2)'></button></input>" +
-"    <input type='button' button class='button-unlabela pure-button' style='width: 125px; height: 40px; color: black' input type='button' value='Opaque' onclick='update_surface(3)'></button></input></ul></div>" + "</td></tr></table>"  
+"    </select>" 
++  "<br><br>"  
 
  //change color scheme 
  var c =   
-"<table border='0'><tr><td width='25%' valign='top'><font style='color:white; font-size:12pt'>Scheme: </font></td><td><font style='color: black; font-size: 8pt; font-style: italic'>Click to change</font><br><select class='styled-select' id='selected_scheme' onchange='update_surface(0)'>" +
+"<font style='color:white; font-size:12pt'>Scheme </font><hr width='75%'><select class='styled-select' id='selected_scheme' onchange='update_surface(0)' style='max-width:50%;'>" +
 "        <option style='color: black;' value='RWB'>Red-White-Blue </option>" +
 "        <option style='color: black;' value='ROYGB'>Red-Green-Blue </option>" +
 "        <option style='color: black;' value='BWR'>Blue-White-Red </option>" +
 "    </select></td></tr></table>" 
 
-var i = 
-"<table border='0'><tr><td width='25%' valign='top'><font style='color:white; font-size:12pt'>Visualization: </font></td><td><div class='inner'><select class='styled-select' id='selected_vis' onchange='set_vis())'>"+
-"        <option style='color: black;' value='stick'>Stick </option>"+
-"        <option style='color: black;' value='line'>Line </option>"+
-"        <option style='color: black;' value='cross'>Cross </option>"+
-"        <option style='color: black;' value='sphere'>Sphere </option>"+
-"        <option style='color: black;' value='cartoon'>Cartoon </option>"+
-"    </select></td></tr></table>"
 
-//change min isoval
-var d =
-" <table class='main' border='0'><tr><td style='color: white; font-size: 12pt'><br>Minimum: &nbsp;</td> " +
-"<td><br><input type='text' id='min_isoval2' value='-5' style='text-align:right; width: 50px; height: 30px; background: transparent; border:0; color: white; font-size: 18px'> </td>" +
-"<td style='color: white; font-size: 18px'><br>kT/e &nbsp;&nbsp;</td>" +
-"<td valign='top'><br><br><input type=range min=-50 max=50 value=-5 id='min_isoval2' step=1 onmouseup='set_min_isoval2(value)'> </td></tr></table>"
 
-//change max isoval
-var e =
-" <table class='main'><tr><td width='38%' style='color: white; font-size: 12pt'>Maximum: &nbsp;</td> " +
-"<td><input type='text' id='max_isoval2' value='5' style='text-align:right; width: 50px; height: 30px; background: transparent; border:0; color: white; font-size: 18px'> </td>" +
-"<td style='color: white; font-size: 18px'>kT/e &nbsp;&nbsp;</td>" +
-"<td valign='top'><br><input type=range min=-50 max=50 value=5 id='max_isoval2' step=1 onmouseup='set_max_isoval2(value)'> </td></tr></table>"
+
 
 //buttons
 var f =
