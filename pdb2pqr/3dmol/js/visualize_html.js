7077cdf4c2f7090afc5f80a8e8c3e9d9c66dabac
karinaliles <lileskr@email.sc.edu>
2015-10-16 11:53:23 -0400

    updated 3dmol ui

--- pdb2pqr/3dmol/js/visualize_html.js
+++ pdb2pqr/3dmol/js/visualize_html.js
@@ -11,7 +11,9 @@ function build_page(jobid){
 	
 	//change surface
     var b = 
-    "<br><font style='color:white; font-size:12pt'><center>Surface</font><hr width='75%'>" +
+    "<br><table border='0' width='100%'><tr><td valign='top'> " +
+    "<font style='color:white; font-size:12pt'>Surface:</font>" + 
+    "</td><td>" +
 "    <select class='styled-select' id='selected_surface' onchange='update_surface(1)' style='max-width:50%;'>" +
 "       <option style='color: black;' value='SAS'>Solvent Accessible</option>" +
 "<option style='color: black;' value='SES'>Solvent Excluded</option>" +
@@ -25,20 +27,23 @@ function build_page(jobid){
 "<div class='inner'><ul class='button-group round' class='rightbutton'><input type='button' button class='button-labela pure-button' style='width: 85px; height: 30px; color: black; font-size: 10pt' value='Translucent' onclick='update_surface(2)'></button></input>" +
 
 "    <input type='button' button class='button-unlabela pure-button' style='width: 85px; height: 30px; color: black' input type='button' value='Opaque' onclick='update_surface(3)'></button></input></ul></div>" +
-
-"<font style='color:white; font-size:12pt'>Isosurface</font><hr width='75%'>" +
-
+"</td></tr>" +
+"<tr><td valign='top'>" +
+"<font style='color:white; font-size:12pt'>Isosurface:</font>" +
+"</td><td>" +
 //change min isoval
-" <p style='color:white; font-size: 16px'> Minimum kT/e <input type=range min=-50 max=50 value=-5 id='min_isoval2' step=1 onmouseup='set_min_isoval2(value)'>&nbsp;&nbsp;&nbsp;&nbsp;<span id='min_isoval'>-5 </span> </p>  " +
+" <p style='color:white; font-size: 16px'> Min<input type=range min=-50 max=50 value=-5 id='min_isoval2' step=1 onmouseup='set_min_isoval2(value)'>&nbsp;&nbsp;&nbsp;&nbsp;<span id='min_isoval'>-5 </span> kT/e </p>  " +
 
 //change max isoval
-" <p style='color:white; font-size: 16px'> Maximum kT/e <input type=range min=-50 max=50 value=5 id='max_isoval2' step=1 onmouseup='set_max_isoval2(value)'>&nbsp;&nbsp;&nbsp;&nbsp;<span id='max_isoval'>5 </span> </p>  " +
+" <p style='color:white; font-size: 16px'> Max<input type=range min=-50 max=50 value=5 id='max_isoval2' step=1 onmouseup='set_max_isoval2(value)'>&nbsp;&nbsp;&nbsp;&nbsp;<span id='max_isoval'> 5 </span> kT/e </p>  " +
 
 //reset button
 "<div class='inner'><ul class='button-group round'></input>" +
-"    <input type='button' button class='button-backbone pure-button' style='width: 85px; height: 30px; color: black' input type='button' value='Reset' onclick='reset_vals()'></button></input></ul></div><br><br>" +
-"<font style='color:white; font-size:12pt'>Model</font><hr width='75%'>" +
-
+"    <input type='button' button class='button-backbone pure-button' style='width: 85px; height: 30px; color: black' input type='button' value='Reset' onclick='reset_vals()'></button></input></ul></div>" +
+"</td></tr>" +
+"<tr><td valign='top'>" +
+"<font style='color:white; font-size:12pt'>Model:</font>&nbsp;&nbsp;" +
+"</td><td>" +
 "<select class='styled-select' id='selected_vis' onchange='set_vis()' style='max-width:50%;'>"+
 "        <option style='color: black;' value='line'>Line </option> "+
 "        <option style='color: black;' value='stick'>Stick </option>" +
@@ -46,24 +51,28 @@ function build_page(jobid){
 "        <option style='color: black;' value='sphere'>Sphere </option>"+
 "        <option style='color: black;' value='cartoon'>Cartoon </option>"+
 "    </select>" 
-+  "<br><br>"  
-
++  "<br><br>"  +
+"</td></tr>" +
+"<tr><td valign='top'>" +
  //change color scheme 
- var c =   
-"<font style='color:white; font-size:12pt'>Scheme </font><hr width='75%'><select class='styled-select' id='selected_scheme' onchange='update_surface(0)' style='max-width:50%;'>" +
+"<font style='color:white; font-size:12pt'>Scheme: </font>" +
+ "</td><td>" +
+"<select class='styled-select' id='selected_scheme' onchange='update_surface(0)' style='max-width:50%;'>" +
+
 "        <option style='color: black;' value='RWB'>Red-White-Blue </option>" +
 "        <option style='color: black;' value='ROYGB'>Red-Green-Blue </option>" +
 "        <!--<option style='color: black;' value='BWR'>Blue-White-Red </option>-->" +
-"    </select><br><br>" 
-
-var h =
-
-"    <div class='inner'><ul class='button-group round'><input type='button' button class='button-labela pure-button' style='width: 85px; height: 30px; color: black' value='Add labels'" +
+"    </select><br><br>" +
+"</td></tr>" +
+"<tr><td valign='top'>" + "</td>" +
+"<td>" +
+"    <div class='inner'><ul class='button-group round'><input type='button' button class='button-labela pure-button' style='width: 90px; height: 30px; color: black' value='Add labels'" +
 "        onclick='addLabels(glviewer); glviewer.render();'></button></input>" +
-"    <input type='button' button class='button-unlabela pure-button' style='width: 85px; height: 30px; color: black' value='Remove labels'" +
+"    <input type='button' button class='button-unlabela pure-button' style='width: 95px; height: 30px; color: black' value='Remove labels'" +
 "        onclick='removetheLabels(glviewer); glviewer.render();'></button></input></ul></div>"  +
 "<div class='inner'><ul class='button-group round'><input type='button' button class='button-backbone pure-button' style='width: 85px; height: 30px; color: black' input type='button' value='Recenter' onclick='glviewer.zoomTo();'></button></input></ul></div>" +
-"<br></div></div>" 
+"<br></div></div>" +
+"</td></tr><table>" 
 
 
 
@@ -71,11 +80,4 @@ var h =
 
 document.write(a)
 document.write(b)
-document.write(c)
-
-
-
-
-document.write(h)
-
 }
