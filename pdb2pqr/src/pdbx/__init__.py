221e99f744af535c0527796e90bfba677b02a682
Nathan Baker <nathanandrewbaker@gmail.com>
2020-04-25 14:44:30 -0700

    Add credits to pdbx.
    Also explain why this isn't handled via PIP.

---
+++ pdb2pqr/src/pdbx/__init__.py
@@ -0,0 +1,8 @@
+"""PDBx
+
+This is a snapshot of the code from
+http://mmcif.wwpdb.org/docs/sw-examples/python/html/index.html
+
+A snapshot of the code was required because PIP does not provide a functional
+Python package for PDBx.
+"""
