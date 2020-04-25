4432c0d87fc97e3fb493947bd849da1cf3423fb5
Nathan Baker <nathanandrewbaker@gmail.com>
2020-04-24 18:59:51 -0700

    Clean up module docstrings and some formatting.

--- pdb2pqr/main.py
+++ pdb2pqr/main.py
@@ -1,51 +1,8 @@
-"""
-    Driver for PDB2PQR
+"""Driver for PDB2PQR
 
-    This module takes a PDB file as input and performs optimizations
-    before yielding a new PDB-style file as output.
-
-    Ported to Python by Todd Dolinsky (todd@ccb.wustl.edu)
-    Washington University in St. Louis
-
-    Parsing utilities provided by Nathan A. Baker (Nathan.Baker@pnl.gov)
-    Pacific Northwest National Laboratory
-
-    Copyright (c) 2002-2020, Jens Erik Nielsen, University College Dublin;
-    Nathan A. Baker, Battelle Memorial Institute, Developed at the Pacific
-    Northwest National Laboratory, operated by Battelle Memorial Institute,
-    Pacific Northwest Division for the U.S. Department Energy.;
-    Paul Czodrowski & Gerhard Klebe, University of Marburg.
-
-	All rights reserved.
-
-	Redistribution and use in source and binary forms, with or without modification,
-	are permitted provided that the following conditions are met:
-
-		* Redistributions of source code must retain the above copyright notice,
-		  this list of conditions and the following disclaimer.
-		* Redistributions in binary form must reproduce the above copyright notice,
-		  this list of conditions and the following disclaimer in the documentation
-		  and/or other materials provided with the distribution.
-        * Neither the names of University College Dublin, Battelle Memorial Institute,
-          Pacific Northwest National Laboratory, US Department of Energy, or University
-          of Marburg nor the names of its contributors may be used to endorse or promote
-          products derived from this software without specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-	IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-	INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-	OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-	OF THE POSSIBILITY OF SUCH DAMAGE.
-
+This module takes a PDB file as input and performs optimizations before yielding a new PDB-style file as output.
 """
 
-__date__  = "5 April 2010"
-__author__ = "Todd Dolinsky, Nathan Baker, Jens Nielsen, Paul Czodrowski, Jan Jensen, Samir Unni, Yong Huang"
 
 import string
 import sys
@@ -74,11 +31,13 @@ from src.aconf import *
 from io import StringIO
 from src.errors import PDB2PQRError
 import propka.lib
+import extensions
+
 
 __version__ = PDB2PQR_VERSION
 
-import extensions
 
+# TODO - needs docstring
 def getOldHeader(pdblist):
     oldHeader = StringIO()
     headerTypes = (HEADER, TITLE, COMPND, SOURCE,
@@ -93,6 +52,8 @@ def getOldHeader(pdblist):
 
     return oldHeader.getvalue()
 
+
+# TODO - needs docstring
 def printPQRHeader(pdblist,
                    atomlist,
                    reslist,
@@ -175,6 +136,8 @@ def printPQRHeader(pdblist,
 
     return header
 
+
+# TODO - needs docstring
 def printPQRHeaderCIF(pdblist,
                       atomlist,
                       reslist,
@@ -277,6 +240,8 @@ def printPQRHeaderCIF(pdblist,
 
     return header
 
+
+# TODO - needs docstring
 def runPDB2PQR(pdblist, ff,
                outname = "",
                ph = None,
@@ -575,6 +540,7 @@ def runPDB2PQR(pdblist, ff,
     return header, lines, missedligandresidues
 
 
+# TODO - needs docstring
 def mainCommand(argv):
     """
         Main driver for running program from the command line.
