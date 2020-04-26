6c1d3d18a48d008c884d3a1010fb6c8ec10465ee
Nathan Baker <nathanandrewbaker@gmail.com>
2020-04-26 10:05:52 -0700

    Move --whitespace test to pytest from Scons.
    See #549.

--- pdb2pqr/pytest/regression_test.py
+++ pdb2pqr/pytest/regression_test.py
@@ -19,19 +19,77 @@ PARSER = cli.build_parser()
             common.DATA_DIR / "1AFS.pdb",
             "output.pqr",
             common.DATA_DIR / "1AFS_ff=AMBER.pqr",
-            id="1AFS local"
+            id="1AFS basic local"
         ),
         pytest.param(
             "--log-level=INFO --ff=AMBER",
             "1AFS",
             "output.pqr",
             common.DATA_DIR / "1AFS_ff=AMBER.pqr",
-            id="1AFS remote"
+            id="1AFS basic remote"
         )
     ]
 )
-def test_fast(args, input_pdb, output_pqr, expected_pqr, tmp_path):
+def test_basic(args, input_pdb, output_pqr, expected_pqr, tmp_path):
     """Basic code to run 1AFS."""
+    arg_str = args + " {inp} {out}"
+    output_pqr = tmp_path / output_pqr
+    _LOGGER.debug("Writing output to %s", output_pqr)
+    arg_str = arg_str.format(inp=input_pdb, out=output_pqr)
+    args = PARSER.parse_args(arg_str.split())
+    main(args)
+    common.compare_pqr(output_pqr, expected_pqr)
+
+
+@pytest.mark.parametrize(
+    "args, input_pdb, output_pqr, expected_pqr",
+    [
+        pytest.param(
+            "--log-level=INFO --whitespace --ff=AMBER",
+            common.DATA_DIR / "1AFS.pdb",
+            "output.pqr",
+            common.DATA_DIR / "1AFS_whitespace_ff=AMBER.pqr",
+            id="1AFS whitespace AMBER"
+        ),
+        pytest.param(
+            "--log-level=INFO --whitespace --ff=CHARMM",
+            common.DATA_DIR / "1AFS.pdb",
+            "output.pqr",
+            common.DATA_DIR / "1AFS_whitespace_ff=CHARMM.pqr",
+            id="1AFS whitespace CHARMM"
+        ),
+        pytest.param(
+            "--log-level=INFO --whitespace --ff=PARSE",
+            common.DATA_DIR / "1AFS.pdb",
+            "output.pqr",
+            common.DATA_DIR / "1AFS_whitespace_ff=PARSE.pqr",
+            id="1AFS whitespace PARSE"
+        ),
+        pytest.param(
+            "--log-level=INFO --whitespace --ff=PEOEPB",
+            common.DATA_DIR / "1AFS.pdb",
+            "output.pqr",
+            common.DATA_DIR / "1AFS_whitespace_ff=PEOEPB.pqr",
+            id="1AFS whitespace PEOEPB"
+        ),
+        pytest.param(
+            "--log-level=INFO --whitespace --ff=SWANSON",
+            common.DATA_DIR / "1AFS.pdb",
+            "output.pqr",
+            common.DATA_DIR / "1AFS_whitespace_ff=SWANSON.pqr",
+            id="1AFS whitespace SWANSON"
+        ),
+        pytest.param(
+            "--log-level=INFO --whitespace --ff=TYL06",
+            common.DATA_DIR / "1AFS.pdb",
+            "output.pqr",
+            common.DATA_DIR / "1AFS_whitespace_ff=TYL06.pqr",
+            id="1AFS whitespace TYL06"
+        )
+    ]
+)
+def test_whitespace(args, input_pdb, output_pqr, expected_pqr, tmp_path):
+    """Basic code to run 1AFS with --whitespace."""
     arg_str = args + " {inp} {out}"
     output_pqr = tmp_path / output_pqr
     _LOGGER.debug("Writing output to %s", output_pqr)
