96beb45ef0f2220ad0c819c0f347b0f9910743f8
Nathan Baker <bake113@WE39447.pnl.gov>
2020-05-05 09:26:00 -0700

    Move PROPKA to submodule.
    This will help with several activities:
    
    * Submitting changes to PROPKA back to developers as pull requests. #564
    * Testing PROPKA. #584.

--- pdb2pqr/pdb2pqr/propka/__init__.py
+++
@@ -1,5 +0,0 @@
-# propka 3.1
-# (Module renamed from original Source to propka to facilitate setuptools installation)
-
-__all__ = ['coupled_residues', 'lib', 'parameters', 'residue', 'bonds', 'debug', 'ligand', 'pdb', 'calculator', 'determinants', 'mutate', 'protein', 'chain', 'iterative', 'output', 'protonate']
-
